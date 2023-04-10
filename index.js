3d56918575dc00ff64ad8abe44e2a5d24a34f859
khanhmhq99 <qkhanhpro99@gmail.com>
2023-04-10 16:04:43 +0700

    change the listener

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(3000, () => {
+	console.log("JSON Server is running");
+});
